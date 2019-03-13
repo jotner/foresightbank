a0d46dd11b0f1002b6f8c6f91677889b4c824b27
andr517 <andreingman38@gmail.com>
2019-03-13 13:51:08 +0100

    Edited Account

--- frontend/components/Account.vue
+++ frontend/components/Account.vue
@@ -12,14 +12,12 @@
       </div>
     </div>
     <div class="columns">
-      <div class="column is-3">
+      <div class="column">
         <div class="panel">
           <p class="panel-heading">
             Privatkonto - 4569kr
           </p>
         </div>
-      </div>
-      <div class="column is-3">
         <div class="panel">
           <p class="panel-heading">
             Fondkonto - 68321kr
@@ -30,103 +28,51 @@
     <div class="columns is-multiline">
       <div class="column">
         <div class="box">
-          <div class="heading">Top Seller Total</div>
-          <div class="title">56,950</div>
+          <div class="heading">Utgifter</div>
+          <div class="title">5434kr</div>
           <div class="level">
             <div class="level-item">
               <div class="">
-                <div class="heading">Sales $</div>
-                <div class="title is-5">250,000</div>
+                <div class="heading">Faktura</div>
+                <div class="title is-5">2500kr</div>
               </div>
             </div>
             <div class="level-item">
               <div class="">
-                <div class="heading">Overall $</div>
-                <div class="title is-5">750,000</div>
+                <div class="heading">Räkningar</div>
+                <div class="title is-5">1500kr</div>
               </div>
             </div>
             <div class="level-item">
               <div class="">
-                <div class="heading">Sales %</div>
+                <div class="heading">Övrigt</div>
+                <div class="title is-5">2500kr</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="box">
+          <div class="heading">Inkomster</div>
+          <div class="title">16343kr</div>
+          <div class="level">
+            <div class="level-item">
+              <div class="">
+                <div class="heading">Arbete</div>
+                <div class="title is-5">60%</div>
+              </div>
+            </div>
+            <div class="level-item">
+              <div class="">
+                <div class="heading">Försäljning</div>
                 <div class="title is-5">25%</div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="box">
-          <div class="heading">Revenue / Expenses</div>
-          <div class="title">55% / 45%</div>
-          <div class="level">
             <div class="level-item">
               <div class="">
-                <div class="heading">Rev Prod $</div>
-                <div class="title is-5">30%</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Rev Serv $</div>
-                <div class="title is-5">25%</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Exp %</div>
-                <div class="title is-5">45%</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="box">
-          <div class="heading">Feedback Activity</div>
-          <div class="title">78% &uarr;</div>
-          <div class="level">
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Positive</div>
-                <div class="title is-5">1560</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Negative</div>
-                <div class="title is-5">368</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Pos/Neg %</div>
-                <div class="title is-5">77% / 23%</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="box">
-          <div class="heading">Orders / Returns</div>
-          <div class="title">75% / 25%</div>
-          <div class="level">
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Orders $</div>
-                <div class="title is-5">425,000</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Returns $</div>
-                <div class="title is-5">106,250</div>
-              </div>
-            </div>
-            <div class="level-item">
-              <div class="">
-                <div class="heading">Success %</div>
-                <div class="title is-5">+ 28,5%</div>
+                <div class="heading">Övrigt</div>
+                <div class="title is-5">15%</div>
               </div>
             </div>
           </div>
@@ -135,3 +81,7 @@
     </div>
   </div>
 </template>
+
+<script>
+
+</script>
