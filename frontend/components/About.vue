36785d9de1480aba72f50e246ef91968843697ef
Andre <andreingman38@gmail.com>
2019-03-17 17:02:14 +0100

    Added frontend to aboutpage

---
+++ frontend/components/About.vue
@@ -0,0 +1,124 @@
+<template>
+  <section class="section" id="about">
+    <div class="section-heading">
+      <h3 class="title is-2">About Us</h3>
+      <h4 class="subtitle is-5">Our mission is to create
+        engaged, lifelong relationships.</h4>
+        <p><strong>We serve nearly half of scandinavia's households with a broad range of financial services, including personal banking,
+          credit cards, mortgages, auto financing, investment advice, small business loans and payment processing.
+        </strong></p>
+        <p>
+        Every day, we provide unmatched convenience, serving approximately 6 million consumer and small business clients. Foresight bank
+         is a global leader in wealth management, corporate and investment banking and trading across a broad range of asset classes,
+          serving corporations, governments, institutions and individuals around the world. The company serves clients through operations
+           in 5 countries.
+         </p>
+    </div>
+
+    <div class="columns has-same-height is-gapless">
+      <div class="column">
+        <div class="card">
+          <div class="card-content">
+            <h3 class="title is-4">Head Office</h3>
+
+            <div class="content">
+              <table class="table-profile">
+                <tr>
+                  <th colspan="1"></th>
+                  <th colspan="2"></th>
+                </tr>
+                <tr>
+                  <td>Address:</td>
+                  <td>Lidlstreet 4b</td>
+                </tr>
+                <tr>
+                  <td>Phone:</td>
+                  <td>0123-456789</td>
+                </tr>
+                <tr>
+                  <td>Email:</td>
+                  <td>Foresightbank@foresight.com</td>
+                </tr>
+              </table>
+            </div>
+            <br>
+            <div class="buttons has-addons is-centered">
+              <a href="#" class="button is-link">Sweden</a>
+              <a href="#" class="button is-link">Denmark</a>
+              <a href="#" class="button is-link">Norway</a>
+              <a href="#" class="button is-link">Finland</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img src="media/bank.jpg" alt="Office building">
+            </figure>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <div class="card">
+          <div class="card-content skills-content">
+            <h3 class="title is-4">Foresightbank stockfound</h3>
+            <div class="content">
+
+              <article class="media">
+                <div class="media-content">
+                  <div class="content">
+                    <p>
+                      <strong>Traction B:</strong>
+                      <br>
+                      <progress class="progress" value="90" max="100"></progress>
+                      <strong class="has-text-success">+7.11% +14.12%</strong>
+                    </p>
+                  </div>
+                </div>
+              </article>
+
+              <article class="media">
+                <div class="media-content">
+                  <div class="content">
+                    <p>
+                      <strong>NAXS:</strong>
+                      <br>
+                      <progress class="progress" value="75" max="100"></progress>
+                    </p>
+                  </div>
+                </div>
+              </article>
+
+              <article class="media">
+                <div class="media-content">
+                  <div class="content">
+                    <p>
+                      <strong>Vostok New Ventures:</strong>
+                      <br>
+                      <progress class="progress" value="65" max="100"></progress>
+                    </p>
+                  </div>
+                </div>
+              </article>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.button.is-link {
+  background-color: #23285b;
+}
+
+</style>
--- frontend/index.js
+++ frontend/index.js
@@ -3,23 +3,29 @@ import App from './components/App.vue'
 import Home from './components/Home.vue'
 import LogIn from './components/LogIn.vue'
 import Account from './components/Account.vue'
+import About from './components/About.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const router = new VueRouter ({
+const router = new VueRouter({
   routes: [{
-    component: Home,
-    path: '/'
-  },
-  {
-    component: LogIn,
-    path: '/login'
-  },
-  {
-    component: Account,
-    path: '/account'
-  }]
+      component: Home,
+      path: '/'
+    },
+    {
+      component: LogIn,
+      path: '/login'
+    },
+    {
+      component: Account,
+      path: '/account'
+    },
+    {
+      component: About,
+      path: '/about'
+    }
+  ]
 })
 
 new Vue({
