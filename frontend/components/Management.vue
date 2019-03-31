592e297d0cc4b6f3831805246310ad08f83046d8
andr517 <andreingman38@gmail.com>
2019-03-31 20:53:42 +0200

    Changed layout of management page

--- frontend/components/Management.vue
+++ frontend/components/Management.vue
@@ -11,115 +11,104 @@
     </div>
   </div>
 
-    <section class="hero">
+    <section class="hero is-white">
       <div class="hero-body">
         <div class="container">
-          <h2 class="title is-4">Transfer</h2>
-          <b-dropdown v-model="isPublic" aria-role="list">
-            <button class="button is-primary" type="button" slot="trigger">
-              <template v-if="isPublic">
-                <b-icon icon="earth"></b-icon>
-                <span>From</span>
-                  </template>
-                  <template v-else>
-                    <b-icon icon="account-multiple"></b-icon>
-                    <span>Friends</span>
-                  </template>
-                  <b-icon icon="menu-down"></b-icon>
-                </button>
+          <h2 class="title is-4">Transfers</h2>
+          <div class="columns has-text-centered">
 
-                <b-dropdown-item :value="true" aria-role="listitem">
-                  <div class="media">
-                    <b-icon class="media-left" icon="earth"></b-icon>
-                    <div class="media-content">
-                      <h3>1</h3>
-                </div>
-                    </div>
-          </b-dropdown-item>
+          <div class="column is-4">
+            <p class="content">
+              <b>From :</b>
+               <strong>{{ radioFrom }}</strong>
+            </p>
 
-          <b-dropdown-item :value="false" aria-role="listitem">
-              <div class="media">
-                  <b-icon class="media-left" icon="account-multiple"></b-icon>
-                  <div class="media-content">
-                      <h3>2</h3>
-                  </div>
-              </div>
-          </b-dropdown-item>
-      </b-dropdown>
-
-      <b-dropdown v-model="isPublic" aria-role="list">
-        <button class="button is-primary" type="button" slot="trigger">
-          <template v-if="isPublic">
-            <b-icon icon="earth"></b-icon>
-            <span>To</span>
-              </template>
-              <template v-else>
-                <b-icon icon="account-multiple"></b-icon>
-                <span>Friends</span>
-              </template>
-              <b-icon icon="menu-down"></b-icon>
-            </button>
-
-            <b-dropdown-item :value="true" aria-role="listitem">
-              <div class="media">
-                <b-icon class="media-left" icon="earth"></b-icon>
-                <div class="media-content">
-                  <h3>1</h3>
-            </div>
-                </div>
-      </b-dropdown-item>
-
-      <b-dropdown-item :value="false" aria-role="listitem">
-          <div class="media">
-              <b-icon class="media-left" icon="account-multiple"></b-icon>
-              <div class="media-content">
-                  <h3>2</h3>
-              </div>
+            <div class="block">
+              <b-radio v-model="radioFrom"
+              native-value="default">
+              default
+            </b-radio>
           </div>
-      </b-dropdown-item>
-  </b-dropdown>
-<a v-on:click="newBankAccountName" class="button">Transfer</a>
-    </div>
-    </div>
-    </section>
+        </div>
 
+        <div class="column is-5">
+          <p class="content">
+            <b>To :</b>
+             <strong>{{ radioTo }}</strong>
+          </p>
 
-  <section class="hero is-white">
-    <div class="hero-body">
-      <div class="container">
-        <b-field label="Name">
-         <b-input v-model="nameInput" placeholder="Enter the name of your new account"></b-input>
-         <a v-on:click="newBankAccountName" class="button">Create account</a>
-       </b-field>
+          <div class="block">
+            <b-radio v-model="radioTo"
+            native-value="default">
+            default
+          </b-radio>
+        </div>
+      </div>
+      <div class="column">
+        <a style="margin-top:25px;" v-on:click="" class="button"><i style="font-size:20px;" class="fa fa-arrow-right"></i></a>
+      </div>
+
+        </div>
       </div>
     </div>
   </section>
 
-  <h2 class="title is-5 has-text-centered">Active accounts</h2>
-  <div
-    v-for="newAccount in newAccounts"
-    :key="newAccount.id"
-    class="list is-hoverable">
-  <a class="list-item">
-    <span>{{newAccount.name}}</span>
-      <a v-on:click="deleteBankAccount(newAccount.id)" class="button is-danger">Delete</a>
-  </a>
-</div>
+  <div id="securehero" class="hero is-small">
+    <div class="hero-body secbg">
+      <div class="container has-text-centered">
+        <h2 class="title-index">
+          Customize your account to suit your needs
+        </h2>
+      </div>
+    </div>
+  </div>
 
+  <section class="hero is-white">
+    <div class="hero-body">
+      <div class="container">
+        <h2 class="title is-4">Create new account</h2>
+        <div class="columns">
+          <div class="column">
+        <b-field>
+         <b-input style="width:500px;" v-model="nameInput" placeholder="Name of account"></b-input>
+         <a v-on:click="newBankAccountName" class="button">Create account</a>
+       </b-field>
+     </div>
+     </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="hero is-white">
+    <div class="hero-body">
+      <div class="container">
+        <h2 class="title is-4">Active accounts</h2>
+        <div
+        v-for="newAccount in newAccounts"
+        :key="newAccount.id"
+        class="list is-hoverable">
+        <a class="list-item">
+          <p class="title is-5"><span class="has-text-weight-light">Account name | </span>{{newAccount.name}}</p>
+          <p class="title is-5"><span class="has-text-weight-light">Account balance | </span>{{newAccount.balance}}$</p>
+          <a v-on:click="deleteBankAccount(newAccount.id)" style="" class="button is-danger">Delete</a>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
   </section>
 </template>
 
 <script>
   export default {
     created() {
-      fetch('/api/account').then(response => response.json()) // Fetching accountInfo from/account and stores the json object in this.user key
+      fetch('/api/account/').then(response => response.json()) // Fetching accountInfo from/account and stores the json object in this.user key
         .then(result => {
           this.user = result
         }),
       fetch('/api/registeraccount/').then(response => response.json()) // Fetching accountInfo from/account and stores the json object in this.user key
         .then(result => {
           this.newAccounts = result
-          console.log(this.newAccounts)
         })
     },
 
@@ -129,7 +118,8 @@
         nameInput: null,
         newAccounts: null,
         deletedAccount: null,
-        isPublic: true,
+        radioFrom:null,
+        radioTo:null,
       }
     },
     methods: {
@@ -158,4 +148,5 @@
 </script>
 
 <style scoped>
+
 </style>
